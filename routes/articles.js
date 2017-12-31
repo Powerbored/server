294e39ffd2e2886cd95b8750842f526c6f49b726
Powerbored <andrew.daniel.lewis@gmail.com>
2017-12-31 14:32:31 +1100

    Full basic CRUD for articles with mongoose and mongoDB

--- routes/articles.js
+++ routes/articles.js
@@ -1,34 +1,94 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/serverdb');
+let db = mongoose.connection;
+db.once('open', function() {
+	console.log('Connected to Mongodb' + db);
+});
+db.on('error', function(err){
+	console.log(err);
+});
+
+let articleModel = require('../models/article');
 
 router.get('/', function(req, res) {
-	let articles = [
-		{
-			id: 1,
-			title: 'Article One',
-			author: 'Andrew',
-			body: 'This is atricle one'
-		}, {
-			id: 2,
-			title: 'Article Two',
-			author: 'not Andrew',
-			body: 'This is atricle two'
-		}, {
-			id: 3,
-			title: 'Article Three',
-			author: 'Andrew',
-			body: 'This is atricle three'
+	articleModel.find({}, function(err, articles){
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('articles/index', {
+				title: 'Articles',
+				articles: articles
+			});
 		}
-	];
-	res.render('articles/index', {
-		title: 'Articles',
-		articles: articles
 	});
 });
 
 router.get('/add', function(req, res) {
 	res.render('articles/add', { 
 		title: 'Add Article' 
+	});
+});
+
+router.post('/add', function(req, res){
+	let article = new articleModel();
+	article.title = req.body.title;
+	article.author = req.body.author;
+	article.body = req.body.body;
+	article.save(function(err){
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			res.redirect('/articles');
+		}
+	});
+});
+
+router.get('/:id', function(req, res){
+	articleModel.findById(req.params.id, function(err, article){
+		res.render('articles/article', {
+			article: article
+		});
+	});
+});
+
+router.get('/edit/:id', function(req, res){
+	articleModel.findById(req.params.id, function(err, article){
+		res.render('articles/edit', {
+			article: article
+		});
+	});
+});
+
+router.post('/edit/:id', function(req, res){
+	let article = {
+		title: req.body.title,
+		author: req.body.author,
+		body: req.body.body
+	},
+	query = {_id: req.params.id};
+
+	articleModel.update(query, article, function(err){
+		if (err) {
+			console.log(err);
+			return;
+		} else {
+			res.redirect('/articles');
+		}
+	});
+});
+
+router.delete('/:id', function(req, res) {
+	let query = {_id:req.params.id};
+
+	articleModel.remove(query, function(err) {
+		if (err) {
+			console.log(err);
+		}
+		res.send('Success');
 	});
 });
 
---
+++ static/js/article.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+	deleteArticle.addEventListener('click', function(e){
+		const id = e.target.getAttribute('data-id');
+		fetch('/articles/'+id, {
+			method: 'DELETE',
+			headers: new Headers(),
+			mode: 'cors',
+			cache: 'default'
+		}).then(function(res) {
+			if (res.ok) {
+				window.location = '/articles';
+			} else {
+				console.log(res.status || 'There was no response from the server');
+			}
+		}).catch(function(err) {
+			console.log(err);
+		})
+	});
+};
